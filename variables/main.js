d4fb5caa11b0a6d5a82344e6d1065fa094214e98
Alex Kolpakov <svetodiod88@mail.ru>
2019-11-11 20:47:53 +0300

    + 2 works

--- variables/main.js
+++ variables/main.js
@@ -3,3 +3,8 @@ let geography = 4;
 let physics = 4;
 let averageRaring =(algebra + geography + physics) / 3;
 console .log(averageRaring); 
+
+
+let myName = 'Alex';
+let message = ('Привет, мир! Меня зовут + ${myName}');
+console .log(message);
