2c3e6d801b3743c2ee2ba65ba418176bfe729fea
Alex Kolpakov <svetodiod88@mail.ru>
2019-11-25 22:13:18 +0300

    1st mistake

--- Extended-Syntax/task.js
+++ Extended-Syntax/task.js
@@ -4,16 +4,26 @@ function calculateQuadraticEquation(){
     let a = +window.a.value;
     let b = +window.b.value;
     let c = +window.c.value;
-    let result = getResult(a,b,c);
-    window.equation.textContent = `${a}*x^2 + (${b})*x + (${c}) = 0`;
+    let result = getResult(a,b,c);  (${c}) = 0`;
     let span = window.result;
     span.textContent = "х = "+result;
 }
 
 function getResult(a,b,c){
+    
     // код для задачи №1 писать здесь
     //return x;
-}
+    пусть d = b * d - 4 * a * c;
+    if (d > 0) {
+        return [( - b + Math. sqrt (d)) / ( 2 * a ), ( - b - Math . sqrt (d)) / ( 2 * a)];
+            } иначе если (d < 0 ){
+                возврат [ ноль ];
+            } else { // d = 0
+            } return [ - b / ( 2 * a)];
+};
+console .log ( getResult (2 , -10 , 4)); // d>0
+console .log ( getResult ( 1 , 1 , 1 )); // d<0
+console .log ( getResult ( 2 , - 4 , 2)); // d=0 
 
 function calculateAverageRating(){
     let marks = window.marks.value.split("").map(Number).filter((n)=> !isNaN(n) && n > 0);
